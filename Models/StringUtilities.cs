122a02f413eb55b29b20eca8fd6813a9217a97ef
MOTENE JO <#Mj49994>
2024-07-22 11:45:57 +0200

    Added Models and the file

---
+++ Models/StringUtilities.cs
@@ -0,0 +1,18 @@
+﻿namespace StringManipulationAPI.Models
+{
+    public static class StringUtilities
+    {
+        public static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            string reversed = ReverseString(input);
+            return input.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
